c4f228199481d8aa30e6956cbdb4695aaf18f07a
maksymbohachov85 <120776559+maksymbohachov85@users.noreply.github.com>
2024-04-08 23:24:38 +0300

    add HW2

---
+++ Homework2/hm2.js
@@ -0,0 +1,100 @@
+// 1. Завдання "Якщо змінна більше нуля - виведіть true, менше - false //Перевірте це на варіантах  1, 0, -3." //
+
+
+          // const num = prompt("Введіть цифру");
+          // if (num >= 0) {
+          //   console.log("true");
+          // } else if (num <= 0) {
+          //   console.log("false");
+          // }
+
+
+
+// Якщо змінна = "test" - виведіть true, //Перевірте це на варіантах  'test', "qwerty", true //
+
+          // let text = "test";
+          // if (text === "test") {
+          //   console.log("true");
+          // } else if (text === "qwerty") {
+          //   console.log("false")
+          // }
+
+// Якщо змінна більше 10 - відніміть 5, //менше - додайте 5, результат виведіть в консоль //Перевірте це на варіантах  1, 10, 13. //
+
+            
+            
+                    // const num = (arr) => {
+                    //   for (i in arr) {
+                    //     if (arr[i] > 10) {
+                    //       console.log(arr[i] - 5);
+                    //     } else if (arr[i] < 10) {
+                    //       console.log(arr[i] + 5);
+                    //     } else {
+                    //       console.log (arr[i])
+                    //     }
+                    //   }
+                    // }
+                    // const numArr = [1, 10, 13];
+                    // num(numArr);
+                    // console.log(num)
+
+
+// Зробіть сервіс який отримує число від 1 до 12 // виведіть місяць який дорівнює числу //
+
+                // const month = prompt("Введіть назву місяця").toLocaleLowerCase();
+                // let num;
+
+                // switch (month) {
+                //   case "січень":
+                //     num = 1;
+                //     break;
+                //   case "лютий":
+                //     num = 2;
+                //     break;
+                //   case "березень":
+                //     num = 3;
+                //     break;
+                //   case "квітень":
+                //     num = 4;
+                //     break;
+                //   case "травень":
+                //     num = 5;
+                //     break;
+                //   case "червень":
+                //     num = 6;
+                //     break;
+                //   case "липень":
+                //     num = 7;
+                //     break;
+                //   case "серпень":
+                //     num = 8;
+                //     break;
+                //   case "вересень":
+                //     num = 9;
+                //     break;
+                //   case "жовтень":
+                //     num = 10;
+                //     break;
+                //   case "листопад":
+                //     num = 11;
+                //     break;
+                //   case "грудень":
+                //     num = 12;
+                //     break;
+                // }
+
+                // alert(`${num}`);
+
+// Зробіть сервіс який отримує тризначне число //Поверніть користувачу сумму цих чисел
+
+
+              // const input = prompt("Введіть три цифри:");
+              // const array = input.split("");
+              // array.length = 3;
+              // let sum = 0;
+
+              // for (let i = 0; i < array.length; i++) {
+              // sum += parseInt(array[i]);
+              // }
+
+              // console.log("Сума: " + sum);
